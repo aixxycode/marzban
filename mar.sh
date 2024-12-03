45cc06d4da9a7446daa001772cb890fe952ebb6e
aixxycode <zkendev@gmail.com>
2024-12-03 13:49:24 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -263,18 +263,18 @@ curl https://get.acme.sh | sh -s email=$email
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /var/lib/marzban/xray.crt --keypath /var/lib/marzban/xray.key --ecc
 wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/aixxycode/marzban/main/xray_config.json"
 
-#install firewall
-#apt install ufw -y
-#sudo ufw default deny incoming
-#sudo ufw default allow outgoing
-#sudo ufw allow ssh
-##sudo ufw allow http
-#sudo ufw allow https
-#sudo ufw allow 8081/tcp
-#sudo ufw allow 1080/tcp
-#sudo ufw allow 1080/udp
-#sudo ufw allow $port/tcp
-#yes | sudo ufw enable
+install firewall
+apt install ufw -y
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw allow 8081/tcp
+sudo ufw allow 1080/tcp
+sudo ufw allow 1080/udp
+sudo ufw allow $port/tcp
+yes | sudo ufw enable
 
 #install database
 wget -O /var/lib/marzban/db.sqlite3 "https://github.com/aixxycode/marzban/raw/main/db.sqlite3"
